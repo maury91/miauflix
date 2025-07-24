907298c8387187e443ed862f90880aa24878054f
Maurizio Carboni <maury91@gmail.com>
2025-07-24 19:04:05 +0000

    Fixed edge case with humanReadableBytes

--- backend/src/utils/numbers.ts
+++ backend/src/utils/numbers.ts
@@ -1,5 +1,8 @@
 export const humanReadableBytes = (bytes: bigint | number): string => {
-  const units = ['B', 'KB', 'MB', 'GB', 'TB'];
+  if (bytes < 1n) {
+    return '0 B';
+  }
+  const units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
   const unitIndex = Math.floor(Math.log2(Number(bytes)) / 10);
   const value = Number(bytes) / Math.pow(1024, unitIndex);
   return `${value.toFixed(2)} ${units[unitIndex]}`;
