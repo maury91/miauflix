e2b7ccb3ffc742adc99db5618d8285982582f00c
Maurizio Carboni <maury91@gmail.com>
2025-08-17 09:49:21 +0000

    Fix

--- frontend/src/app/hooks/usePrevious.ts
+++ frontend/src/app/hooks/usePrevious.ts
@@ -1,9 +1,9 @@
 import { useEffect, useRef } from 'react';
 
 export const usePrevious = <T>(value: T) => {
-  const ref = useRef<T>();
+  const ref = useRef<T>(null);
   useEffect(() => {
     ref.current = value;
   });
-  return ref.current;
+  return ref.current ?? value;
 };
